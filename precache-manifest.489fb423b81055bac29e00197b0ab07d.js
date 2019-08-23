fe981de0946154b364c406c0204f95ef6cee95db
hesong <actkhs@gmail.com>
2019-08-24 00:51:10 +0800

    Updates

---
+++ precache-manifest.489fb423b81055bac29e00197b0ab07d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b3808491e72669c584d936c237944a",
+    "url": "/connect-the-x/index.html"
+  },
+  {
+    "revision": "2c0da8e60ad3a6dc06a8",
+    "url": "/connect-the-x/static/css/main.ad65400c.chunk.css"
+  },
+  {
+    "revision": "55387ed8e9580e4d7f19",
+    "url": "/connect-the-x/static/js/2.52021481.chunk.js"
+  },
+  {
+    "revision": "2c0da8e60ad3a6dc06a8",
+    "url": "/connect-the-x/static/js/main.098cda8f.chunk.js"
+  },
+  {
+    "revision": "1b9d61e34f80405ffad0",
+    "url": "/connect-the-x/static/js/runtime~main.d89b0f77.js"
+  },
+  {
+    "revision": "7a25f1999a15760508f5e6131245aeac",
+    "url": "/connect-the-x/static/media/hero-banner.7a25f199.jpg"
+  },
+  {
+    "revision": "9325e4fcdb0ed935ac5994df7c89cd46",
+    "url": "/connect-the-x/static/media/job-hero-banner-320W.9325e4fc.jpg"
+  },
+  {
+    "revision": "9b2c7aceaa4a7085a5b36f7cbd0641cc",
+    "url": "/connect-the-x/static/media/job-hero-banner-480W.9b2c7ace.jpg"
+  },
+  {
+    "revision": "60a740d1f5aee7552b9b4a8e30c798ab",
+    "url": "/connect-the-x/static/media/job-hero-banner-720W.60a740d1.jpg"
+  },
+  {
+    "revision": "8b9a5cd1abdf533be8bf4a1af84fa3ee",
+    "url": "/connect-the-x/static/media/spinner.8b9a5cd1.gif"
+  }
+]);
